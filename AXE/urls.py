b76916930e8c1e67b8601c5dd4071607e04b0951
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2023-03-19 12:27:29 -0600

    Commit inical

---
+++ AXE/urls.py
@@ -0,0 +1,25 @@
+"""AXE URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+#el resultado de la url seria: 127.0.0.1/axe/permisos --->ejemplo
+#por tanto la url de su aplicacion seria nombre_su_modulo/
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('axe/', include('seg_permisos.urls')),
+]
